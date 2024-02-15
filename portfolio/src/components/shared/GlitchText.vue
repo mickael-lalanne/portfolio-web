18b77598a4047d3bf16d36d281e65cd33c44d0f1
Mickaël Lalanne <mickael.lalanne03@gmail.com>
2024-02-15 16:51:47 -0500

    Improve glitched text effect

--- portfolio/src/components/shared/GlitchText.vue
+++ portfolio/src/components/shared/GlitchText.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="container">
-        <div class="glitch" :data-text="text">{{ text }}</div>
+    <div class="container" :style="`--content-var: '${text}';`">
+        <div class="content">
+            {{ text }}
+        </div>
     </div>
 </template>
 
@@ -17,81 +19,105 @@ export default {
 
 <style lang="scss" scoped>
 .container {
-    position: relative;
-    white-space: nowrap;
-    font-family: "Oswald", sans-serif;
-    font-family: 'Black Ops One', system-ui;
-    font-style: italic;
     text-align: center;
-    // transform: translate(-50%, -50%);
-    // top: 40%;
-    // left: 50%;
 }
 
-.glitch {
-    color: rgb(223, 191, 191);
+.content,
+.content::after {
+    padding: 10px 50px;
+    font-size: 20px;
+    border: none;
+    border-radius: 5px;
     color: rgb(var(--v-theme-primary));
+    background-color: transparent;
     position: relative;
+    font-family: 'Black Ops One', system-ui;
     font-size: 42px;
-    // margin: 70px 200px;
-    animation: glitch 5s 5s infinite;
 }
 
-.glitch::before {
-    content: attr(data-text);
+.content::after {
+    --move1: inset(50% 50% 50% 50%);
+    --move2: inset(31% 0 40% 0);
+    --move3: inset(39% 0 15% 0);
+    --move4: inset(45% 0 40% 0);
+    --move5: inset(45% 0 6% 0);
+    --move6: inset(14% 0 61% 0);
+    clip-path: var(--move1);
+    content: var(--content-var);
     position: absolute;
-    left: -2px;
-    // text-shadow: -5px 0 magenta;
-    text-shadow: -5px 0 magenta;
-    // background: black;
-    overflow: hidden;
     top: 0;
-    width: 100%;
-    animation: noise-1 3s linear infinite alternate-reverse,
-        glitch 5s 5.05s infinite;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: block;
 }
 
-.glitch::after {
-    content: attr(data-text);
-    position: absolute;
-    left: 2px;
-    text-shadow: -5px 0 lightgreen;
-    // background: black;
-    overflow: hidden;
-    top: 0;
-    width: 100%;
-    animation: noise-2 3s linear infinite alternate-reverse,
-        glitch 5s 5s infinite;
+.content::after {
+    animation: glitch_4011 5s infinite;
+    text-shadow: 10 10px 10px black;
+    animation-timing-function: steps(2, end);
+    text-shadow: -3px -3px 0px #1df2f0, 3px 3px 0px #e94be8;
+    background-color: transparent;
 }
 
-@keyframes glitch {
-    // 1% {
-    //     transform: rotateX(10deg) skewX(90deg);
-    // }
-    // 2% {
-    //     transform: rotateX(0deg) skewX(0deg);
-    // }
-}
-
-@keyframes noise-1 {
-    $steps: 30;
-    @for $i from 1 through $steps {
-        #{percentage($i*(1/$steps))} {
-            $top: random(100);
-            $bottom: random(101 - $top);
-            clip-path: inset(#{$top}px 0 #{$bottom}px 0);
-        }
+@keyframes glitch_4011 {
+    0% {
+        clip-path: var(--move1);
+        transform: translate(0px, -10px);
     }
-}
 
-@keyframes noise-2 {
-    $steps: 30;
-    @for $i from 0 through $steps {
-        #{percentage($i*(1/$steps))} {
-            $top: random(100);
-            $bottom: random(101 - $top);
-            clip-path: inset(#{$top}px 0 #{$bottom}px 0);
-        }
+    2% {
+        clip-path: var(--move2);
+        transform: translate(-10px, 10px);
+    }
+
+    4% {
+        clip-path: var(--move3);
+        transform: translate(10px, 0px);
+    }
+
+    6% {
+        clip-path: var(--move4);
+        transform: translate(-10px, 10px);
+    }
+
+    8% {
+        clip-path: var(--move5);
+        transform: translate(10px, -10px);
+    }
+
+    10% {
+        clip-path: var(--move6);
+        transform: translate(-10px, 10px);
+    }
+
+    12% {
+        clip-path: var(--move1);
+        transform: translate(10px, -10px);
+    }
+
+    14% {
+        clip-path: var(--move3);
+        transform: translate(-10px, 10px);
+    }
+
+    16% {
+        clip-path: var(--move2);
+        transform: translate(10px, -10px);
+    }
+
+    18% {
+        clip-path: var(--move4);
+        transform: translate(-10px, 10px);
+    }
+
+    20% {
+        clip-path: var(--move1);
+        transform: translate(0);
+    }
+    100% {
+        clip-path: var(--move1);
+        transform: translate(0);
     }
 }
 </style>
